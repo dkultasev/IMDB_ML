65c512a058899dd3d7f84ca926d101ab9f1f20c6
Dmitrij Kultasev <dkultasev@gmail.com>
2018-09-11 10:05:33 +0300

    Add rating update procedure and movie list function

---
+++ ML_IMDB/dbo/Functions/MoviesWithRatingsByUserName.sql
@@ -0,0 +1,17 @@
+﻿CREATE FUNCTION dbo.MoviesWithRatingsByUserName
+(
+    @CustomerReviewerName NVARCHAR(200)
+)
+RETURNS TABLE
+AS
+    RETURN (
+               SELECT      t.TitleId
+                         , Title
+                         , AvgRating
+                         , NbrVotes
+                         , cr.CustomReviewerId
+                         , utr.Rating
+                 FROM      title              AS t
+                 LEFT JOIN dbo.CustomReviewer AS cr ON cr.CustomReviewerName = @CustomerReviewerName
+                 LEFT JOIN UserTitleRating    AS utr ON utr.TitleId          = t.TitleId
+                                                    AND utr.CustomReviewerId = cr.CustomReviewerId);
---
+++ ML_IMDB/dbo/Procedures/pUpdateCustomReviewerTitleRating.sql
@@ -0,0 +1,27 @@
+﻿CREATE PROCEDURE dbo.pUpdateCustomReviewerTitleRating
+    @CustomerReviewerName NVARCHAR(200)
+  , @TitleId              CHAR(9)
+  , @Rating               DECIMAL(4, 2)
+AS
+    BEGIN
+        DECLARE @ReviewerId INT;
+
+        SELECT @ReviewerId = cr.CustomReviewerId
+          FROM dbo.CustomReviewer AS cr
+         WHERE cr.CustomReviewerName = @CustomerReviewerName;
+
+        IF (@ReviewerId IS NULL)
+            BEGIN
+                RAISERROR(
+                    15600
+                  , -1
+                  , -1
+                  , 'Exception in [dbo].[pUpdateCustomReviewerTitleRating]: Custom reviewer is not found.');
+            END;
+
+        UPDATE dbo.UserTitleRating
+           SET Rating = @Rating
+         WHERE TitleId = @TitleId AND CustomReviewerId = @ReviewerId;
+
+        SELECT @@ROWCOUNT AS rows_updated;
+    END;
