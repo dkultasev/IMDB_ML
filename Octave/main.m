b6050be37d68ae4e4bf0566a8ab2175ec3c3c5e3
Dmitrij Kultasev <dkultasev@gmail.com>
2018-09-28 10:11:01 +0300

    First version of algorithm

--- Octave/main.m
+++ Octave/main.m
@@ -5,16 +5,42 @@ try
     data = load('data.txt');
     rating = data(:,1);
     features = data(:,2:end);
+    k = 15;
+    
     
     durationNormalized = featureNormalize(features(:,1));
+    yearCodeNormalized = featureNormalize(features(:,2));
     features(:,1) = durationNormalized;
+    features(:,2) = yearCodeNormalized;
 
-    K = 54;
+    cinemaIndex = int8(rand(1)*rows(features)/2);
+    cinemaRating = rating(cinemaIndex);
+    cinema = features(cinemaIndex,:);
 
-    randomIndexes = randperm(rows(features));
-    randomK = features(randomIndexes(1:K),:);
+    euclidian = sqrt(sum((features.-cinema).^2, 2));
 
+    euclidianRatings = [rating euclidian];
 
+    #skipping first row as it always be the same film as it was taken from the same subset
+
+    sortedDistancesWithRatings = sortrows(euclidianRatings, 2);
+    
+    cinemaRating
+    calculatedRating = mean(sortedDistancesWithRatings(2:k+1,1))
+    
+    
+
+    
+
+    #euclidian = vecnorm(features, duplicatedSingleFeature)
+
+    
+    
+    
+    #randomIndexes = randperm(rows(features));
+    #randomK = features(randomIndexes(1:K),:);
+
+    "FINISH"
     pause
 catch ME
   ME
