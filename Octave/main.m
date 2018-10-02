75e272a03d59444a46be4404f879462bd70e35cb
Dmitrij Kultasev <dkultasev@gmail.com>
2018-10-02 10:58:27 +0300

    Add standard error validation

--- Octave/main.m
+++ Octave/main.m
@@ -2,47 +2,72 @@ clear ; close all; clc
 graphics_toolkit('fltk');
 
 try
+
     data = load('data.txt');
-    rating = data(:,1);
-    features = data(:,2:end);
-    k = 15;
+    test_data = load('test.txt');
     
+    rating = data(:,1);
+    test_rating = test_data(:,1);
+    features = data(:,2:end);
+    test_features = test_data(:,2:end);
+    
+    from_k = 15;
+    K = 25;
     
     durationNormalized = featureNormalize(features(:,1));
     yearCodeNormalized = featureNormalize(features(:,2));
     features(:,1) = durationNormalized;
     features(:,2) = yearCodeNormalized;
 
-    cinemaIndex = int8(rand(1)*rows(features)/2);
-    cinemaRating = rating(cinemaIndex);
-    cinema = features(cinemaIndex,:);
+    test_durationNormalized = featureNormalize(test_features(:,1));
+    test_yearCodeNormalized = featureNormalize(test_features(:,2));
+    test_features(:,1) = test_durationNormalized;
+    test_features(:,2) = test_yearCodeNormalized;
 
-    euclidian = sqrt(sum((features.-cinema).^2, 2));
+    loops = rows(test_features);
+    is_first = 1;
 
-    euclidianRatings = [rating euclidian];
+    for k = from_k:K
+      for i = 1:loops
+        cinemaRating = test_rating(i);
+        cinema = test_features(i);
 
-    #skipping first row as it always be the same film as it was taken from the same subset
+        euclidian = sqrt(sum((features.-cinema).^2, 2));
 
-    sortedDistancesWithRatings = sortrows(euclidianRatings, 2);
+        euclidianRatings = [rating euclidian];
+
+        sortedDistancesWithRatings = sortrows(euclidianRatings, 2);
+
+        calculatedRating = mean(sortedDistancesWithRatings(1:k+1,1));
+      
+        if i == 1
+          Answer = [cinemaRating calculatedRating];
+        else
+          Answer = [Answer; [cinemaRating calculatedRating]];
+        endif
+      # [cinemaRating calculatedRating]
+      end
+      Y = Answer(:,1);
+      Y_predicted = Answer(:,2);
+
+      o = sqrt(sum((Y-Y_predicted).^2)/rows(Y));
+
+      if (is_first==1)
+          standard_error = o;
+          is_first = 0;
+      else
+          standard_error = [standard_error; o];
+      endif  
+      k  
+    end
+
     
-    cinemaRating
-    calculatedRating = mean(sortedDistancesWithRatings(2:k+1,1))
-    
-    
 
-    
-
-    #euclidian = vecnorm(features, duplicatedSingleFeature)
-
-    
-    
-    
-    #randomIndexes = randperm(rows(features));
-    #randomK = features(randomIndexes(1:K),:);
-
+    save first_try.mat standard_error
     "FINISH"
     pause
 catch ME
   ME
   pause
 end    
+
